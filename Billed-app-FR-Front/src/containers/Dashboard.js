2cb073b77a33475b6a056cf9c3e7f5cc17e60b0b
Olivia L <letendartolivia@gmail.com>
2023-04-21 13:14:22 +0200

    move the forEach loop + create a const containing the filtered cards to loop on it and solve the bug

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -86,10 +86,7 @@ export default class {
     }
 
     handleEditTicket(e, bill, bills) {
-        if (this.counter === undefined || this.counter > 0 || this.id !== bill.id) this.counter = 0
-        console.log('le counter : ' + this.counter);
-        console.log('id de bill : ' + bill.id);
-        console.log('l"id:' + this.id);
+        if (this.counter === undefined || this.id !== bill.id) this.counter = 0
         if (this.id === undefined || this.id !== bill.id) this.id = bill.id
         if (this.counter % 2 === 0) {
             bills.forEach(b => {
@@ -137,20 +134,23 @@ export default class {
         if (this.counter === undefined || this.index !== index) this.counter = 0
         if (this.index === undefined || this.index !== index) this.index = index
         if (this.counter % 2 === 0) {
+            const billFiltered = filteredBills(bills, getStatus(this.index));
             $(`#arrow-icon${this.index}`).css({transform: 'rotate(0deg)'})
             $(`#status-bills-container${this.index}`)
                 .html(cards(filteredBills(bills, getStatus(this.index))))
             this.counter++
+
+            billFiltered.forEach(bill => {
+                $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+            })
+
+
         } else {
             $(`#arrow-icon${this.index}`).css({transform: 'rotate(90deg)'})
             $(`#status-bills-container${this.index}`)
                 .html("")
             this.counter++
         }
-
-        bills.forEach(bill => {
-            $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-        })
 
         return bills
 
