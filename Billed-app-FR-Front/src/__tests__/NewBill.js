a53d8ea876e9e0a12f1e81419687e44c5d0a50cb
Olivia L <letendartolivia@gmail.com>
2023-05-31 15:18:03 +0200

    create new test in the file NewBills.js in the folder test : creation of a test verifying that you receive an error if you insert a file other than png, jpg or jpeg + create a test to check that calling the updateBill function redirects you to billsUI

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -3,19 +3,18 @@
  */
 
 import {fireEvent, screen, waitFor} from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-import mockStore from "../__mocks__/store.js"
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
-import {ROUTES_PATH, ROUTES} from "../constants/routes.js";
-
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import mockStore from "../__mocks__/store.js"
 
 describe("Given I am connected as an employee", () => {
     describe("When I am on NewBill Page", () => {
         describe("When I change file to a valid file", () => {
-            test('Then Should call method handleChangeFile', async () => {
-                Object.defineProperty(window, localStorage, {value: localeStorageMock})
+            // Not in the coverage
+            test("Then Should call method handleChangeFile", async () => {
+                Object.defineProperty(window, 'localStorage', {value: localStorageMock})
                 window.localStorage.setItem('user', JSON.stringify({
                     type: 'Employee'
                 }))
@@ -25,25 +24,24 @@ describe("Given I am connected as an employee", () => {
                 router()
                 window.onNavigate(ROUTES_PATH.NewBill)
 
-                const onNavigate = pathname => {
-                    document.body.innerHTML = ROUTES({pathname})
-                }
+                const onNavigate = (pathname) => {
+                    document.body.innerHTML = ROUTES({pathname});
+                };
 
                 let NewBillInstance = new NewBill({
                     document,
                     onNavigate,
                     store: null,
-                    localStorage: window.localStorage
-                })
+                    localStorage: window.localStorage,
+                });
 
-                const spyHandleChangeFile = jest.spyOn(NewBillInstance, "handleChangeFile")
+                const spyHandleChangeFile = jest.spyOn(NewBillInstance, 'handleChangeFile')
 
                 let changeFileBtn = await waitFor(() => screen.getByTestId('file'))
 
                 fireEvent.click(changeFileBtn, () => {
-                    expect(spyHandleChangeFile).toHaveBeenCalled()
+                    expect(spyHandleChangeFile).toHaveBeenCalled();
                 });
-
             })
 
             test("Then should create a new file", async () => {
@@ -52,57 +50,156 @@ describe("Given I am connected as an employee", () => {
                     type: 'Employee',
                     email: 'Employee@billed.com'
                 }))
-
                 const root = document.createElement("div")
                 root.setAttribute("id", "root")
                 document.body.append(root)
                 router()
-                window.onNavigate((ROUTES_PATH.NewBill))
+                window.onNavigate(ROUTES_PATH.NewBill)
 
-                const onNavigate = pathname => {
-                    document.body.innerHTML = ROUTES({pathname})
-                }
+                const onNavigate = (pathname) => {
+                    document.body.innerHTML = ROUTES({pathname});
+                };
 
                 let NewBillInstance = new NewBill({
                     document,
                     onNavigate,
                     store: mockStore,
-                    localStorage: window.localStorage
-                })
+                    localStorage: window.localStorage,
+                });
 
                 let billMocked = {
-                    "id": "UnnsnsKDPAnsajjiwemqneodjna",
-                    "name": "test666",
+                    "id": "UIUZtnPQvnbFnB0ozvJh",
+                    "name": "test66",
                     "email": "a@a",
                     "type": "Services en ligne",
                     "vat": "60",
                     "pct": 20,
-                    "commentAdmin": "ça marche !",
+                    "commentAdmin": "bon bah d'accord",
                     "amount": 300,
                     "status": "accepted",
-                    "date": "2023-05-31",
+                    "date": "2003-03-03",
                     "commentary": "",
                     "fileName": "facture-client-php-exportee-dans-document-pdf-enregistre-sur-disque-dur.png",
                     "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…dur.png?alt=media&token=571d34cb-9c8f-430a-af52-66221cae1da3"
                 }
 
                 const fileList = {0: billMocked, length: 1, item: () => billMocked};
-                const fileInput = {files: fileList, value: "test.png"};
+                const fileInput = {files: fileList, value: 'test.png'};
                 const changeEvent = {preventDefault: jest.fn(), target: fileInput};
 
                 const mockStoreCreateSpy = jest.spyOn(mockStore.bills(), 'create');
                 await waitFor(() => jest.fn(NewBillInstance.handleChangeFile(changeEvent)))
 
                 expect(mockStoreCreateSpy).toHaveBeenCalled();
-
             })
         })
 
         describe("When I change file to a not valid file", () => {
             test("Then should return an error message", async () => {
-                
+                Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+                window.localStorage.setItem('user', JSON.stringify({
+                    type: 'Employee',
+                    email: 'Employee@billed.com'
+                }))
+                const root = document.createElement("div")
+                root.setAttribute("id", "root")
+                document.body.append(root)
+                router()
+                window.onNavigate(ROUTES_PATH.NewBill)
+
+                const onNavigate = (pathname) => {
+                    document.body.innerHTML = ROUTES({pathname});
+                };
+
+                let NewBillInstance = new NewBill({
+                    document,
+                    onNavigate,
+                    store: mockStore,
+                    localStorage: window.localStorage,
+                });
+
+                let billMocked = {
+                    "id": "UIUZtnPQvnbFnB0ozvJh",
+                    "name": "test3",
+                    "email": "a@a",
+                    "type": "Services en ligne",
+                    "vat": "60",
+                    "pct": 20,
+                    "commentAdmin": "bon bah d'accord",
+                    "amount": 300,
+                    "status": "accepted",
+                    "date": "2003-03-03",
+                    "commentary": "",
+                    "fileName": "facture-client-php-exportee-dans-document-pdf-enregistre-sur-disque-dur.png",
+                    "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…dur.png?alt=media&token=571d34cb-9c8f-430a-af52-66221cae1da3"
+                }
+
+                const fileList = {0: billMocked, length: 1, item: () => billMocked};
+                const fileInput = {files: fileList, value: 'test.aaaa'};
+                const changeEvent = {preventDefault: jest.fn(), target: fileInput};
+
+                await waitFor(() => jest.fn(NewBillInstance.handleChangeFile(changeEvent)))
+                expect(screen.getAllByText("Veuillez entrer un fichier de type jpg, jpeg ou png.")).toBeTruthy();
             })
         })
 
+        describe("When I sumbit form with valid data", () => {
+            test("Then should call updateBill and redirect to billsUI", async () => {
+                Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+                window.localStorage.setItem('user', JSON.stringify({
+                    type: 'Employee',
+                    email: 'Employee@billed.com'
+                }))
+                const root = document.createElement("div")
+                root.setAttribute("id", "root")
+                document.body.append(root)
+                router()
+                window.onNavigate(ROUTES_PATH.NewBill)
+
+                const onNavigate = (pathname) => {
+                    document.body.innerHTML = ROUTES({pathname});
+                };
+
+                let NewBillInstance = new NewBill({
+                    document,
+                    onNavigate,
+                    store: mockStore,
+                    localStorage: window.localStorage,
+                });
+
+                // replace updateBill by a mock function
+                NewBillInstance.updateBill = jest.fn();
+
+                // event
+                let billMocked = {
+                    "id": "UIUZtnPQvnbFnB0ozvJh",
+                    "name": "test3",
+                    "email": "a@a",
+                    "type": "Services en ligne",
+                    "vat": "60",
+                    "pct": 20,
+                    "commentAdmin": "bon bah d'accord",
+                    "amount": 300,
+                    "status": "accepted",
+                    "date": "2003-03-03",
+                    "commentary": "",
+                    "fileName": "facture-client-php-exportee-dans-document-pdf-enregistre-sur-disque-dur.png",
+                    "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…dur.png?alt=media&token=571d34cb-9c8f-430a-af52-66221cae1da3"
+                }
+
+                const fileList = {0: billMocked, length: 1, item: () => billMocked};
+                const fileInput = {
+                    files: fileList,
+                    value: 'test.png',
+                    querySelector: (selector) => document.querySelector(selector)
+                };
+                const changeEvent = {preventDefault: jest.fn(), target: fileInput};
+
+                await waitFor(() => jest.fn(NewBillInstance.handleSubmit(changeEvent)))
+
+                expect(NewBillInstance.updateBill).toHaveBeenCalled();
+                expect(screen.getByText("Mes notes de frais")).toBeTruthy()
+            })
+        })
     })
 })
